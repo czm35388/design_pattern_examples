eb0bd2fdc391132652c8550b1d6d9638ff8da521
czm35388 <michael.harald.1990@gmail.com>
2023-01-25 15:13:12 +0100

    * added functionality to fully be able to parse mapfiles

--- src/creational_patterns/factory_method/map_parser.cpp
+++ src/creational_patterns/factory_method/map_parser.cpp
@@ -7,8 +7,6 @@ std::unique_ptr<cParser> cMapParser::create()
 
 config_storage cMapParser::parse_file(std::string strFileName)
 {
-    std::cout << "Mapfile - Parser parse configuration file: " << strFileName.c_str() << std::endl;
-
     pugi::xml_document oMapfile;
     pugi::xml_parse_result result = oMapfile.load_file(strFileName.c_str());
 
@@ -16,24 +14,65 @@ config_storage cMapParser::parse_file(std::string strFileName)
     {
         std::cerr << "XML parsing failed! Error description: " << result.description() << std::endl;
     }
-    else
+
+    config_storage oMeasurementConfig = {};
+
+    for(const auto& oOutputPin : oMapfile.child("mapping").child("outputs").children("output"))
     {
-        std::cout << "XML parsing of mapfile succeeded!" << std::endl;
+        const std::string strDaqEvent = oOutputPin.attribute("daq_event").as_string();
+        const std::string strPollingInterval = oOutputPin.attribute("polling_interval").as_string();
+        std::string strDaqPrescaller = oOutputPin.attribute("daq_prescaler").as_string();
+
+        std::string strMethodName = {""};
+        if(!strDaqEvent.empty())
+        {
+            if(strDaqPrescaller.empty())
+            {
+                strDaqPrescaller.append("1");
+            }
+
+            strMethodName.append("DAQ " + strDaqEvent + " (" + strDaqPrescaller + ")");
+        }
+        else
+        {
+            strMethodName.append("Polling " + strPollingInterval + " [ms]");
+        }
+
+        std::vector<std::string> oSignals = {};
+        for(const auto& oSignal : oOutputPin.children("assignment"))
+        {
+            std::string strSignalName = {""};
+
+            if(!std::string(oSignal.child_value("raw_value")).compare("true"))
+            {
+                strSignalName.append("raw.");
+            }
+
+            strSignalName.append(oSignal.child_value("to"));
+            oSignals.push_back(strSignalName);
+        }
+
+        // if the MeasurementMethod already exists in the Config
+        // we merge the signals into the existing one
+        if (oMeasurementConfig.contains(strMethodName)) 
+        {
+            for(const auto& oSignal : oSignals)
+            {
+                // check if the signal alreay exists in the MeasurementMethod
+                // if not add it to the Method
+                std::vector<std::string> oVector = oMeasurementConfig[strMethodName];
+                if(std::find(oVector.begin(), oVector.end(), oSignal) == oVector.end())
+                {
+                    oMeasurementConfig[strMethodName].push_back(oSignal);
+                }
+            }
+        } 
+        else 
+        {
+            oMeasurementConfig.insert(std::pair{strMethodName, oSignals});
+        }
+
     }
 
-    handle_outputs_nodes(oMapfile.children("output"));
-
-    // @todo
-    return config_storage();
-}
-
-void cMapParser::handle_outputs_nodes(pugi::xml_object_range<pugi::xml_named_node_iterator> output_nodes)
-{
-    uint16_t nOutputNodeCount = { 0 }; 
-    for(const auto output_node : output_nodes)
-    {
-        nOutputNodeCount += 1;
-    }
-
-    std::cout << "Number of OutputNodes: " << nOutputNodeCount << std::endl;
+    return oMeasurementConfig;
 }
